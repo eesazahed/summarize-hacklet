97c65005847b2bdda3aabd9c2b33e5fc9c0dbf28
Eesa Zahed <eesa.zahed@gmail.com>
2025-04-24 15:57:27 -0400

    added 404

--- app.py
+++ app.py
@@ -9,15 +9,13 @@ API_URL = "https://ai.hackclub.com/chat/completions"
 
 @app.route("/")
 def home():
-    return {
-        "message": "check out https://github.com/eesazahed/summarize-hacklet"
-    }
+    return "<p>check out <a href='https://github.com/eesazahed/summarize-hacklet'>github.com/eesazahed/summarize-hacklet</a></p>", 200
 
 @app.route("/api", methods=["POST"])
 def summarize():
     data = request.get_json()
-    if not data["text"]:
-        return { "message": "error: no text provided" }
+    if not data or "text" not in data:
+        return { "message": "error: no text provided" }, 500
     
     prompt = f"Summarize this:\n\n{data['text']}"
 
@@ -31,10 +29,14 @@ def summarize():
         response_data = response.json()
         summary = response_data["choices"][0]["message"]["content"]
         
-        return { "summary": summary }
+        return { "summary": summary }, 200
 
     except Exception as e:
-        return { "message": f"error: {str(e)}" }
+        return { "message": f"error: {str(e)}" }, 500
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<p>page not found<br /><br /><a href='https://github.com/eesazahed/summarize-hacklet'>github.com/eesazahed/summarize-hacklet</a></p>", 404
 
 if __name__ == "__main__":
     app.run(debug=True)
